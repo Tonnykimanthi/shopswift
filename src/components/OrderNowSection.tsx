03a45981e591c681fa6608dd164ce018fb08e4f2
Tony Km <kimaxhanthony9@gmail.com>
2024-01-10 18:34:19 +0300

    Add: OrderNowSection.tsx, Edit: ProductDetails.tsx

---
+++ src/components/OrderNowSection.tsx
@@ -0,0 +1,17 @@
+const OrderNowSection = () => {
+  return (
+    <aside className="-mt-4 p-3 bg-slate-100 max-lg:mt-5">
+      <h4 className="font-medium text-lg text-center max-lg:text-start max-lg:underline underline-offset-2">Shipping Details</h4>
+      <div className="leading-tight space-y-2 mt-2">
+        <p>Free shipping on all orders above $200</p>
+        <p>Simply return within 30 days for an exchange</p>
+        <p>Pay with a secure payment for all your orders</p>
+      </div>
+      <button className="block mt-4 mx-auto text-white font-medium w-52 py-1.5 max-lg:w-full max-w-64 bg-primary-orange hover:shadow-lg hover:shadow-primary-orange/40 transition">
+        ORDER NOW
+      </button>
+    </aside>
+  );
+};
+
+export default OrderNowSection;
--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -5,6 +5,7 @@ import useFetch from "../hooks/useFetch";
 import { Link, useParams } from "react-router-dom";
 import AddToCartBtn from "../components/AddToCartBtn";
 import LikeButton from "../components/LikeButton";
+import OrderNowSection from "../components/OrderNowSection";
 
 type productProps = {
   id: number;
@@ -34,67 +35,68 @@ const ProductDetails = () => {
   }, [data]);
 
   return (
-    <div className="mt-5 px-8 flex flex-col items-center">
+    <div className="mt-5 px-8 flex flex-col items-center max-lg:px-5">
       {loading && <Loader />}
       {!loading && error ? (
         <ErrorMessage />
       ) : (
         <>
           {product && (
-            <main className="p-4 bg-slate-50">
-              <section className="flex gap-x-5 bg-slate-50">
-                <aside className="w-1/3 max-w-72 overflow-hidden">
+            <main className="py-4 bg-slate-50">
+              <section className="flex gap-x-2 max-md:flex-col max-md:items-center bg-slate-50">
+                <div className="w-1/3 max-w-72 p-2 overflow-hidden max-md:w-full">
                   <img
-                    className="object-cover w-full h-full"
+                    className="object-cover w-full"
                     src={product.image}
                     alt={product.title}
                   />
-                </aside>
-
-                <div>
-                  {/* Title */}
-                  <div className="flex -mt-2 gap-x-2">
-                    <h2 className="-mt-0.5 text-3xl font-medium leading-tight">
-                      {product.title}
-                    </h2>
-                    <LikeButton />
-                  </div>
-                  <div className="mt-2">
-                    <span className="block text-3xl font-bold">
-                      ${product.price}
-                    </span>
-                    <span className="mt-2 block text-lg italic font-medium">
-                      Category:{" "}
-                      <Link
-                        to={`/${product.category}`}
-                        className="not-italic text-primary-blue hover:underline underline-offset-2 capitalize"
-                      >
-                        {product.category}
-                      </Link>
-                    </span>
-                    <span className="text-lg italic font-medium">
-                      Rating:{" "}
-                      <small className="text-lg text-primary-green font-medium">
-                        {product.rating.rate}
-                      </small>
-                    </span>
-                  </div>
-                  <div className="mt-5 w-full">
-                    <AddToCartBtn />
-                  </div>
                 </div>
 
-                <aside className="-mt-4 p-3 bg-slate-100">
-                  <button className="text-white w-52 py-1 bg-primary-orange/80 hover:bg-primary-orange transition">
-                    Order now
-                  </button>
-                </aside>
-              </section>
+                <div className="w-2/3 px-2 flex flex-grow max-lg:flex-col divide-y-2 max-md:w-full">
+                  <div>
+                    {/* Title */}
+                    <div className="flex -mt-2 gap-x-2 max-md:mt-0">
+                      <h2 className="text-2xl font-medium leading-tight">
+                        {product.title}
+                      </h2>
+                      <LikeButton />
+                    </div>
+                    <div className="mt-2">
+                      <span className="block text-3xl font-bold">
+                        ${product.price}
+                      </span>
+                      <span className="mt-2 block text-lg italic font-medium">
+                        Category:{" "}
+                        <Link
+                          to={`/${product.category}`}
+                          className="not-italic text-primary-blue hover:underline underline-offset-2 capitalize"
+                        >
+                          {product.category}
+                        </Link>
+                      </span>
+                      <span className="text-lg italic font-medium">
+                        Rating:{" "}
+                        <small className="text-lg text-primary-green font-medium">
+                          {product.rating.rate}
+                        </small>
+                      </span>
 
-              {/* Product Description */}
-              <section className="mt-8">
-                <h3 className="text-2xl font-medium">Product Description</h3>
-                <p className="mt-2 w-2/3">{product.description}</p>
+                      {/* Product Description */}
+                      <section className="mt-5">
+                        <h3 className="text-xl font-medium underline underline-offset-2">
+                          Product Description
+                        </h3>
+                        <p className="mt-1">{product.description}</p>
+                      </section>
+                    </div>
+                    <div className="mt-5 w-full">
+                      <AddToCartBtn />
+                    </div>
+                  </div>
+
+                  <OrderNowSection />
+                </div>
+
               </section>
             </main>
           )}
