6f4f9b19c7ce76123bec9c55b1742eac25cb6dc1
Tony Km <kimaxhanthony9@gmail.com>
2024-02-11 21:35:54 +0300

    Edit ShippingInfo.tsx: Adding styles for each icon

--- src/layouts/ShippingInfo.tsx
+++ src/layouts/ShippingInfo.tsx
@@ -1,6 +1,6 @@
 import { IconType } from "react-icons";
 // Icons
-import { FcShipped } from "react-icons/fc";
+import { MdOutlineLocalShipping } from "react-icons/md";
 import { MdCurrencyExchange } from "react-icons/md";
 import { RiSecurePaymentLine } from "react-icons/ri";
 
@@ -8,35 +8,39 @@ interface dataProps {
   icon: IconType;
   title: string;
   description: string;
+  styles: string;
 }
 
 const ShippingInfo = () => {
   const shippingData: dataProps[] = [
     {
-      icon: FcShipped,
+      icon: MdOutlineLocalShipping,
       title: "Free Shipping",
       description: "Free shipping on all orders above $200",
+      styles: "w-12 h-12 fill-primary-blue",
     },
     {
       icon: MdCurrencyExchange,
       title: "30 Days Return",
       description: "Simply return within 30 days for an exchange",
+      styles: "w-11 h-11 fill-primary-blue",
     },
     {
       icon: RiSecurePaymentLine,
       title: "100% Payment Secure",
       description: "Pay with a secure payment for all your orders",
+      styles: "w-12 h-12 fill-primary-blue",
     },
   ];
 
   return (
-    <section className="bg-slate-100 py-5 flex justify-center items-center gap-x-3 gap-y-5 max-sm:flex-col border-b border-slate-300">
+    <section className="flex items-center justify-center gap-x-3 gap-y-5 border-b border-slate-300 bg-slate-100 py-5 max-sm:flex-col">
       {shippingData.map((item, index) => (
         <article key={index} className="flex items-center gap-x-3">
-          <item.icon className="w-12 h-12 fill-primary-blue" />
+          <item.icon className={`${item.styles}`} />
           <div>
             <h3 className="text-xl font-bold leading-5">{item.title}</h3>
-            <p className="mt-1 leading-4 max-w-56">{item.description}</p>
+            <p className="mt-1 max-w-56 leading-4">{item.description}</p>
           </div>
         </article>
       ))}
